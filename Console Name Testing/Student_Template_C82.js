60eb48d7bf126de91e10ec218e7657e1aaceedd3
Hamaffin <101374544+Hamaffin@users.noreply.github.com>
2022-05-05 20:20:55 -0400

    Add files via upload

---
+++ Console Name Testing/Student_Template_C82.js
@@ -0,0 +1,24 @@
+console.log("Console Name Testing");
+
+            var first_name = "Rishaan";
+            console.log(first_name);
+            var last_name = "Devchoudhury";
+            console.log(last_name);
+
+            var full_name = first_name.concat(last_name);
+            console.log(full_name);
+
+            var a = 7;
+            var b = 12;
+
+            var add = a + b;
+            console.log(add);
+
+            var sub = a - b;
+            console.log(sub);
+
+            var multiple = a * b;
+            console.log(multiple);
+
+            var divide = a / b ;
+            console.log(divide);
